00e7ddc7883e5069ad6adc127e69701e1aaff24d
F1GIAN <91794502+F1GIAN@users.noreply.github.com>
2021-11-14 18:39:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
+   
+    int n=0;
+    cin >> n;
+
+    if (n<2){
+        cout << "errore" << endl;
+
+    }else{
+        int a=0, b=1, c=a+b;
+        cout << "1" << endl;
+
+        for (int i=1; i<n; i++){
+            c=a+b;
+            a=b;
+            b=c;
+            cout << c << endl;
+        }
+    }
+
    return 0;
 }
